0006590fb90b436b5008b5549e10c5a0bc0e5d74
zhulifengGit002 <1369687107@qq.com>
2020-01-20 10:42:51 +0800

    all

--- db/db.js
+++ db/db.js
@@ -5,8 +5,8 @@ const url = "mongodb+srv://text:db123456789@cluster0-vsvcn.mongodb.net/test?retr
 function searchUser(whereStr, callBack) {
     mongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true}, function (err, db) {
         if (err) throw err;
-        var dbo = db.db('node_runoob');
-        dbo.collection('site').find(whereStr).toArray(function (err, result) {
+        var dbo = db.db('nodeData');
+        dbo.collection('user').find(whereStr).toArray(function (err, result) {
             if (err) throw err;
             console.log('所查询的数据', result);
             callBack(result);
@@ -18,9 +18,9 @@ function searchUser(whereStr, callBack) {
 function insertUser(myobj, callBack) {
     mongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true}, function (err, db) {
         if (err) throw err;
-        var dbo = db.db('node_runoob');
+        var dbo = db.db('nodeData');
         //插入数据
-        dbo.collection('site').insertOne(myobj, function (err, res) {
+        dbo.collection('user').insertOne(myobj, function (err, res) {
             if (err) throw err;
             console.log('插入数据成功');
             callBack(res);
@@ -32,8 +32,8 @@ function insertUser(myobj, callBack) {
 function searchBlogPost(whereStr, callBack) {
     mongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true}, function (err, db) {
         if (err) throw err;
-        var dbo = db.db('node_blogPost');
-        dbo.collection('site').find(whereStr).toArray(function (err, result) {
+        var dbo = db.db('nodeData');
+        dbo.collection('post_blog').find(whereStr).toArray(function (err, result) {
             if (err) throw err;
             console.log('所查询的数据', result);
             callBack(result);
@@ -45,12 +45,26 @@ function searchBlogPost(whereStr, callBack) {
 function insertBlogPost(myobj, callBack) {
     mongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true}, function (err, db) {
         if (err) throw err;
-        var dbo = db.db('node_blogPost');
+        var dbo = db.db('nodeData');
         //插入数据
-        dbo.collection('site').insertOne(myobj, function (err, res) {
+        dbo.collection('post_blog').insertOne(myobj, function (err, res) {
             if (err) throw err;
-            console.log('插入章数据成功');
+            console.log('插入文章数据成功');
             callBack(res);
+            db.close();
+        })
+    })
+}
+
+//删除文章remove() 方法已经过时了，现在官方推荐使用 deleteOne() 和 deleteMany() 方法。
+function deleteBlogPost(whereStr, callBack) {
+    mongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology:true}, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('nodeData');
+        dbo.collection('post_blog').deleteOne(whereStr, function(err, result){
+            if (err) throw err;
+            console.log('删除文章数据成功', result);
+            callBack(result);
             db.close();
         })
     })
@@ -60,3 +74,4 @@ exports.searchUser = searchUser;
 exports.insertUser = insertUser;
 exports.searchBlogPost = searchBlogPost;
 exports.insertBlogPost = insertBlogPost;
+exports.deleteBlogPost = deleteBlogPost;
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-//const cors = require('cors') // 解决跨域
+const express = require("express")
+const multer  = require('multer')
+const bodyParser = require("body-parser")
+const cors = require('cors') // 解决跨域
 const path = require('path')
-
+const fs = require("fs");
 const db = require('./db/db');
 const app = express();
 
 //创建application/x-www-form-urlencoded编码解析
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-//app.use(cors())
+app.use(cors())
 //app.use(bodyParser.json())
 
-//设置跨域访问
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+// 设置跨域访问
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By",' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
+// });
 //用户注册
 app.post('/register', urlencodedParser, function (req, res) {
   var userName = req.body.userName;
@@ -52,11 +53,12 @@ app.post('/register', urlencodedParser, function (req, res) {
 app.post('/login', urlencodedParser, function (req, res) {
   var userName = req.body.userName;
   var pwd = req.body.pwd; 
+  console.log(userName)
   if (!userName) {
       res.json({ code: -1, message: '用户名不能为空' });
   } else if (!pwd) {
       res.json({ code: -1, message: '密码不能为空' });
-  } else {
+  } else {     
       db.searchUser({ name: userName }, function (result) {
           if (result.length > 0) {
               if (result[0].name == userName && result[0].pwd == pwd) {
@@ -83,7 +85,7 @@ app.post('/blog/addPost', urlencodedParser, function(req, res){
     } else if (!postContent) {
         res.json({ code: -1, message: '文章内容不能为空' });
     } else {
-        db.insertBlogPost({ tltle: postTitle,  sort:postSort, label:postLabel, content:postContent, timestamp:postDate}, function (insertResult) {
+        db.insertBlogPost({ title: postTitle,  sort:postSort, label:postLabel, content:postContent, timestamp:postDate}, function (insertResult) {
             console.log(insertResult)
                 if (insertResult.insertedCount > 0) {
                     res.json({ code: 0, message: '添加成功' });
@@ -103,8 +105,60 @@ app.post('/blog/searchPost', urlencodedParser, function (req, res) {
             res.json({ code: -1, message: '未查询到' });
         }
     });
-  })
-const server = app.listen(8081, function () {
+})
+
+//删除博客文章 不懂为什么使用 timestamp _id 删除失败
+app.post('/blog/deletePost', urlencodedParser, function (req, res) {
+    var title = req.body.title||'';
+    if(!title){
+        res.json({ code: -1, message: '文章不能为空' });
+    }else{
+        db.deleteBlogPost({ title: title}, function (result) {
+            console.log(result)
+            if (result.deletedCount === 1) {
+                res.json({ code: 0, message: '删除文档' });
+            } else {
+                res.json({ code: -1, message: '删除失败' });
+            }
+        });
+    }
+    
+})
+
+//自己创建
+const uploadFolder = './public/uploads/';
+// 通过 filename 属性定制
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+    },
+    filename: function (req, file, cb) {
+        // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+        cb(null, Date.now()+ '-' +  file.originalname);  
+    }
+});
+var upload = multer({ storage: storage })
+//var upload = multer({ dest: "public/uploads/" });
+//文件上传
+app.post('/upload', upload.single('imgFile'), function(req, res, next){
+    var file = req.file;
+    console.log(file)
+    console.log('文件类型：%s', file.mimetype);
+    console.log('原始文件名：%s', file.originalname);
+    console.log('文件大小：%s', file.size);
+    console.log('文件保存路径：%s', file.path);
+    var url = 'http://47.104.215.238:5002/public/uploads/' + file.filename
+    console.log(url)
+    res.send({code: '0', message: '上传成功', url: url});
+})
+
+//获取图片
+app.get('/public/uploads/*', function (req, res) {
+    res.sendFile( __dirname + "/" + req.url );
+    console.log("Request for " + req.url + " received.");
+})
+
+const server = app.listen(5002, function () {
 
   var host = server.address().address
   var port = server.address().port
